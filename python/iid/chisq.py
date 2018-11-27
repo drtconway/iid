2262b18a298ae9186d0b358c2623ec609591d66d
Thomas Conway <thomas.conway@petermac.org>
2018-11-28 09:49:51 +1100

    Chi-squared done.

--- python/iid/chisq.py
+++ python/iid/chisq.py
@@ -18,20 +18,20 @@ class chisq(dist):
         return 2*self.k
 
     def pdf(self, x, **args):
-        assert(self.k == 1 and x > 0)
-        assert(self.k > 1 and x >= 0)
+        assert(self.k != 1 or x > 0)
+        assert(self.k <= 1 or x >= 0)
         k = self.k
         lx = math.log(x)
         l2 = math.log(2)
-        lr = (0.5*k - 1) * lx  - 0.5*x - 0.5*k*l2 - basic.logFamma(0.5*k)
+        lr = (0.5*k - 1) * lx  - 0.5*x - 0.5*k*l2 - basic.logGamma(0.5*k)
         if 'log' in args and args['log']:
             return lr
         else:
             return math.exp(lr)
 
     def cdf(self, x, **args):
-        assert(self.k == 1 and x > 0)
-        assert(self.k > 1 and x >= 0)
+        assert(self.k != 1 or x > 0)
+        assert(self.k <= 1 or x >= 0)
         k = self.k
         if 'upper' in args and args['upper']:
             lr = special.logGammaQ(0.5*k, 0.5*x)
---
+++ python/iid/tests/test_chisq.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+from iid.chisq import chisq
+
+def same(a, b, eps):
+    if a != 0:
+        return abs((a - b)/a) < eps
+    return abs(a - b) < eps
+
+def test_binom():
+    wd = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(wd, 'data', 'dist', 'chisq.yaml')) as f:
+        data = yaml.load(f)['data']
+    for itm in data:
+        n = itm[0]
+        x = itm[1]
+        m = itm[2]
+        v = itm[3]
+        rP = itm[4]
+        rL = itm[5]
+        rU = itm[6]
+        dst = chisq(n)
+        assert same(m, dst.mean(), 1e-14)
+        assert same(v, dst.var(), 1e-14)
+        rP0 = dst.pdf(x)
+        if not same(rP, rP0, 1e-13):
+            print(itm, abs((rP-rP0)/rP))
+        assert same(rP, rP0, 1e-13)
+        rL0 = dst.cdf(x)
+        assert same(rL, rL0, 1e-13)
+
